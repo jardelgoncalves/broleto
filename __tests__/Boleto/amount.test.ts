9ca768e033f75570e2b2f62f3c5f8c8f06e0e23a
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-03-21 20:23:35 -0300

    test: add teste para metodo que retorna o valor do boleto

---
+++ __tests__/Boleto/amount.test.ts
@@ -0,0 +1,23 @@
+import { Boleto } from '../../src/index';
+
+describe('Unit test Boleto/amount method', () => {
+  it('should return the amount if the formatted payment slip digitable line is valid', () => {
+    const boleto = new Boleto('34195.00008 01233.203189 64221.470004 5 84410000002000');
+    expect(boleto.amount()).toBe(20.00);
+  });
+
+  it('should return the amount if the digitable payment slip line without format is valid', () => {
+    const boleto = new Boleto('341950000801233.20318964221470004584410000002000');
+    expect(boleto.amount()).toBe(20.00);
+  });
+
+  it('should return the amount of the payment slip if the bar code is valid', () => {
+    const boleto = new Boleto('34195844100000020005000001233203186422147000');
+    expect(boleto.amount()).toBe(20.00);
+  });
+
+  it('should return null if payment slip is invalid', () => {
+    const boleto = new Boleto('11110000101111.11111111111111114584410000002000');
+    expect(boleto.amount()).toBe(null);
+  });
+});
