f8c70401db0fe2a8179149e552c18bc63694354b
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-03-22 00:40:49 -0300

    feat: diferença de dias de uma data para o agora

---
+++ src/utils/differenceForNow.ts
@@ -0,0 +1,15 @@
+export const differenceForNow = (date: Date) => {
+  const dRefNow = new Date();
+
+  const now = new Date(
+    `${dRefNow.getMonth() + 1}/${dRefNow.getDate()}/${dRefNow.getFullYear()}`,
+  );
+
+  const past = new Date(
+    `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`,
+  );
+
+  const diff = Math.abs(now.getTime() - past.getTime());
+  const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+  return days;
+};
