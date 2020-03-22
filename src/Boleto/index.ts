053141650f5439ab8570930291c9c73984e435ab
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-03-22 00:42:21 -0300

    feat: manipulando os tipos de boleto e data de expirtação de um boleto

--- src/Boleto/index.ts
+++ src/Boleto/index.ts
@@ -1,7 +1,59 @@
+import { maskCleaner, typeMapping, differenceForNow } from '../utils/index';
+
 export class Boleto {
   private number: string
 
   constructor(number: string) {
-    this.number = number;
+    this.number = maskCleaner(number);
+  }
+
+  codeType() {
+    if (this.number.length !== 44 || (this.number.length < 46 && this.number.length > 48)) { return 'INVALIDO'; }
+
+    return this.number.length === 44 ? 'CODIGO DE BARRAS' : 'LINHA DIGITAVEL';
+  }
+
+  type() {
+    const type = this.number.substr(0, 2);
+
+    if (Number(type[0]) === 8) {
+      return typeMapping(Number(type));
+    }
+
+    return {
+      type: 'BANCO',
+      subtype: '',
+    };
+  }
+
+  expirationDate() {
+    const codeType = this.codeType();
+    const typePaymentSlip = this.type();
+    const febrabanDate = new Date('1997-10-07');
+
+    let factor: number | null = null;
+
+    if (codeType === 'LINHA DIGITAVEL') {
+      const days = Number(this.number.substr(33, 4));
+      factor = typePaymentSlip.type === 'BANCO'
+        ? days * (24 * 60 * 60 * 1000)
+        : 0;
+    }
+
+    if (codeType === 'CODIGO DE BARRAS') {
+      const days = Number(this.number.substr(5, 4));
+      factor = typePaymentSlip.type === 'BANCO'
+        ? days * (24 * 60 * 60 * 1000)
+        : 0;
+    }
+
+    return factor || factor === 0 ? new Date(febrabanDate.getTime() + factor) : null;
+  }
+
+  expired() {
+    const expirationDate = this.expirationDate();
+    if (!expirationDate) return null;
+
+    return differenceForNow(expirationDate) > 0;
   }
 }
