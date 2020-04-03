d2e3609adfce9573356eb645f47ef4434839db58
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-04-03 09:56:34 -0300

    refactor: corrigindo build e versão da lib

--- rollup.config.js
+++ rollup.config.js
@@ -11,14 +11,10 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
-      exports: 'named',
-      sourcemap: true,
     },
   ],
   plugins: [
