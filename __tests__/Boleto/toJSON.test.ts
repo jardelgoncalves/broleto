67ff3544c89da8ca4ad835c71d54f23f4037848d
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-04-03 00:36:56 -0300

    test: add testes para toJson

---
+++ __tests__/Boleto/toJSON.test.ts
@@ -0,0 +1,47 @@
+import { Boleto } from '../../src/index';
+
+describe('Unit test Boleto/toJSON method', () => {
+  it('should return an object with all the information', () => {
+    const boleto = new Boleto('34195.00008 01233.203189 64221.470004 5 84410000002000');
+    const info = boleto.toJSON();
+
+    expect(info !== null).toBe(true);
+
+    if (info !== null) {
+      expect(info.barcode).toBe('34195844100000020005000001233203186422147000');
+      expect(info.banks).toBe('Itaú Unibanco S.A');
+      expect(info.type).toBe('BANCO');
+      expect(info.expirationDate instanceof Date).toBe(true);
+      expect(info.expired).toBe(true);
+      expect(info.expiredDays > 100).toBe(true);
+      expect(info.amount).toBe(20);
+      expect(info.valid).toBe(true);
+    }
+  });
+
+  it('should return an object with all the information', () => {
+    const boleto = new Boleto('858100000005572503282005800708200789543988924230');
+    const info = boleto.toJSON();
+
+    expect(info !== null).toBe(true);
+
+    if (info !== null) {
+      expect(info.barcode).toBe('85810000000572503282008007082007854398892423');
+      expect(info.banks).toBe('Unknown');
+      expect(info.type).toBe('ARRECADACAO');
+      expect(info.codeType).toBe('LINHA DIGITAVEL');
+      expect(info.expirationDate instanceof Date).toBe(true);
+      expect(info.expired).toBe(false);
+      expect(info.expiredDays).toBe(0);
+      expect(info.amount).toBe(57.25);
+      expect(info.valid).toBe(true);
+    }
+  });
+
+  it('should return null if boleto is invalid', () => {
+    const boleto = new Boleto('34195.00008 01233.203189');
+    const info = boleto.toJSON();
+
+    expect(info).toBe(null);
+  });
+});
