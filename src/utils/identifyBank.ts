89d34524c833246bc6aa3d7ab1572cb53fdc81af
Marcus Vinicius Peixoto Paulo <marcus.paulo@detran.local>
2020-06-29 12:10:57 -0400

    Add: Banco Caixa

--- src/utils/identifyBank.ts
+++ src/utils/identifyBank.ts
@@ -7,7 +7,7 @@ export const identifyBank = (cod: string) => {
     33: 'Banco Santander (Brasil) S.A',
     36: 'Banco Bradesco BBI S.A',
     341: 'Itaú Unibanco S.A',
-    104: 'Caixa Econômica Federal',
+    104: 'Caixa Econ Federal',
   };
 
   return banks[Number(cod)] || 'Unknown';
